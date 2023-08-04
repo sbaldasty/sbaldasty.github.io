c04def3c6a85cb74a8490810eb94ad7b26b9c364
Steven Baldasty <sbaldasty@gmail.com>
2023-08-04 09:24:23 -0400

    mgmt scripts

---
+++ bin/ud-np.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+NAME=`date +"%Y-%m-%d"`
+hugo new --kind notebook-bundle notebook/$NAME
---
+++ content/notebook/2023-08-04/index.md
@@ -0,0 +1,6 @@
+---
+title: 2023-08-04
+date: 2023-08-04
+---
+
+Anything special about today...
