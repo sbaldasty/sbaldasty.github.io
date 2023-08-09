c9d0040c6f2f1c8478da5cbb34daafef4f9cc216
Steven Baldasty <sbaldasty@gmail.com>
2023-08-09 18:41:05 -0400

    script fixes

---
+++ preprocessing/photos/nh-telephone-museum.py
@@ -0,0 +1,21 @@
+from _lib import gallery
+
+path = 'posts/nh-telephone-museum/'
+
+gallery(f'{path}telephone-cable-cross-section', 'PXL_20230318_145708328',
+    x=400, y=800, width=2100, angle=0)
+
+gallery(f'{path}telegraph', 'PXL_20230318_143311081',
+    x=300, y=300, width=3400, angle=0)
+
+gallery(f'{path}automated-switchboard', 'PXL_20230318_151909109',
+    x=1600, y=1750, width=500, angle=-3)
+
+gallery(f'{path}undersea-telephone-cable', 'PXL_20230318_145833859',
+    x=1400, y=0, width=2500, angle=0)
+
+gallery(f'{path}manual-switchboard', 'PXL_20230318_151424827',
+    x=800, y=900, width=1300, angle=-1)
+
+gallery(f'{path}telephone', 'PXL_20230318_144513000',
+    x=1100, y=1500, width=900, angle=-1)
